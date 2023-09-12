a310bfc138853fedebe188085a34a6d9b7f46b07
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-09-12 16:36:45 -0400

    feat: Add all functions
    Add all pages and functions required in the project.
    Makes use of ejs modules instead of common js.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+import http from 'http';
+import { readFile } from 'node:fs/promises';
 
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer(async (req, res) => {
+    if (req.url === '/') {
+        try {
+            const filePath = new URL('./pages/index.html', import.meta.url);
+            const contents = await readFile(filePath, { encoding: 'utf8' });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(contents);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    else if (req.url === '/about') {
+        try {
+            const filePath = new URL('./pages/about.html', import.meta.url);
+            const contents = await readFile(filePath, { encoding: 'utf8' });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(contents);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    else if (req.url === '/contact-me') {
+        try {
+            const filePath = new URL('./pages/contact-me.html', import.meta.url);
+            const contents = await readFile(filePath, { encoding: 'utf8' });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(contents);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    else {
+        try {
+            const filePath = new URL('./pages/404.html', import.meta.url);
+            const contents = await readFile(filePath, { encoding: 'utf8' });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(contents);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+});
+
+server.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
